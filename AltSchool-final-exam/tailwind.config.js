dc3c6b68a9dddbbdc2ede1358735785220d3af2c
fortune <omorodion.fortune@gmail.com>
2023-03-18 23:34:53 +0100

    reconfigured tailwindcss

--- AltSchool-final-exam/tailwind.config.js
+++ AltSchool-final-exam/tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
